57238eb0744562848a001cc92cc7adac629875db
mwingerden <matthew.wingerden@gmail.com>
2026-01-30 17:25:14 -0800

    Refactored gui.py.

--- gui.py
+++ gui.py
@@ -1,6 +1,8 @@
+import file_organization
 import tkinter as tk
 from tkinter.constants import DISABLED
 from tkinter.filedialog import askdirectory
+from tkinter import messagebox
 
 class GUI:
     def __init__(self):
@@ -15,37 +17,40 @@ class GUI:
     def run_gui(self):
         self._set_up_gui()
         self._root.mainloop()
-        return [self._source_folder, self._dest_folder]
 
     def _set_up_gui(self):
         self._root.title("Combine Docs")
 
         frame = tk.Frame(self._root)
-        frame.grid(row=0, column=0)
+        frame.grid(row=0, column=0, padx=10, pady=10)
 
         self._source_entry = tk.Entry(frame, width=150)
-        self._source_entry.grid(row=0, column=0)
-        source_btn = tk.Button(frame, text="Find Source Folder", command=self._find_source_folder)
+        self._source_entry.grid(row=0, column=0, padx=10, pady=10)
+        source_btn = tk.Button(frame,
+                               text="Find Source Folder",
+                               command=lambda: self._find_folder("_source_entry", "_source_folder"))
         source_btn.grid(row=0, column=1)
 
         self._dest_entry = tk.Entry(frame, width=150)
-        self._dest_entry.grid(row=1, column=0)
-        dest_btn = tk.Button(frame, text="Find Destination Folder", command=self._find_dest_folder)
+        self._dest_entry.grid(row=1, column=0, padx=10, pady=10)
+        dest_btn = tk.Button(frame,
+                             text="Find Destination Folder",
+                             command=lambda: self._find_folder("_dest_entry", "_dest_folder"))
         dest_btn.grid(row=1, column=1)
 
-        run_btn = tk.Button(frame, text="Run", command=self._root.destroy)
-        run_btn.grid(row=2, column=0)
+        run_btn = tk.Button(frame, text="Run", command=self._run_file_organizer)
+        run_btn.grid(row=2, column=0, padx=10, pady=10)
 
-    def _find_source_folder(self):
+    def _find_folder(self, entry_widget_attr, folder_attr):
         path = askdirectory()
-        self._source_entry.delete(0, tk.END)
-        self._source_entry.insert(0, path)
-        self._source_entry.config(state=DISABLED)
-        self._source_folder = path
+        getattr(self, entry_widget_attr).delete(0, tk.END)
+        getattr(self, entry_widget_attr).insert(0, path)
+        getattr(self, entry_widget_attr).config(state=DISABLED)
+        setattr(self, folder_attr, path)
 
-    def _find_dest_folder(self):
-        path = askdirectory()
-        self._dest_entry.delete(0, tk.END)
-        self._dest_entry.insert(0, path)
-        self._dest_entry.config(state=DISABLED)
-        self._dest_folder = path
+    def _run_file_organizer(self):
+        if self._source_folder and self._dest_folder:
+            file_organization.FileOrganization(self._source_folder, self._dest_folder)
+            self._root.destroy()
+        else:
+            tk.messagebox.showwarning("Warning", "Please select both folders!")
--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-import os
-import file_organization
 import gui
 
-gui = gui.GUI()
-folder_list = gui.run_gui()
-
-print(folder_list)
-
-# root.mainloop()
-# source_file_path = os.path.join(os.getcwd(), "Source Files")
-# destination_folder = os.path.join(os.getcwd(), "Modified Files")
-
-# source_file_path = os.path.join(os.getcwd(), "Test Source Files")
-# destination_folder = os.path.join(os.getcwd(), "Test Modified Files")
-
-file_organization.FileOrganization(folder_list[0], folder_list[1])
+gui.GUI().run_gui()
